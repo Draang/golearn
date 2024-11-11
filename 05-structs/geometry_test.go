8cf73c79d60470ca8960ec891d4da105d88e22f8
MICREDIVALE\emmanuel.contreras <dearrollo5gv@outlook.com>
2024-11-11 12:03:13 -0600

    se agrega table driven test para area

--- 05-structs/geometry_test.go
+++ 05-structs/geometry_test.go
@@ -2,6 +2,9 @@ package structs
 
 import "testing"
 
+/**
+table driven tests: Given a table of test cases, the actual test simply iterates through all table entries and for each entry performs the necessary tests. The test code is written once and amortized over all table entries, so it makes sense to write a careful test with good error messages.
+*/
 func TestPerimeter(t *testing.T) {
 	checkPerimeter := func(t testing.TB, shape Shape, want float64) {
 		t.Helper()
@@ -23,22 +26,19 @@ func TestPerimeter(t *testing.T) {
 
 }
 func TestArea(t *testing.T) {
-	checkArea := func(t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
+
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{10.0, 10.0}, 100.0},
+		{Circle{10.0}, 314.15926535897932384626433832795},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
 		}
 	}
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		want := 100.0
-		checkArea(t, rectangle, want)
-
-	})
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10.0}
-		want := 314.15926535897932384626433832795
-		checkArea(t, circle, want)
-	})
 }
