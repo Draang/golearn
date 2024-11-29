cdc863ecd6efb330fa2603a0360ce263e6418190
MICREDIVALE\emmanuel.contreras <dearrollo5gv@outlook.com>
2024-11-29 11:47:32 -0600

    Pointers, nil, Errors, more types

---
+++ 06-pointers/wallet.go
@@ -0,0 +1,39 @@
+package pointers
+
+import (
+	"errors"
+	"fmt"
+)
+
+type Bitcoin int
+type Stringer interface {
+	String() string
+}
+type Wallet struct {
+	balance Bitcoin
+}
+
+// El Asterisco representa que estas mandando la direccion de memoria un puntero de esa manera se cambia el mismo objeto
+func (w *Wallet) Deposit(amount Bitcoin) {
+	//w.balance+=amount <== metodo no efectivo por que la w es una copia de la wallet no modificamos la misma direccion de memoria
+	/*
+		(w Wallet)
+		address of balance in Deposit is 0xc00010c1c8
+		address of balance in test is 0xc00010c1c0
+		fmt.Printf("address of balance in Deposit is %p \n", &w.balance)
+	*/
+	w.balance += amount
+}
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return errors.New("no puedes retirar mas de tu balance")
+	}
+	w.balance -= amount
+	return nil
+}
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
